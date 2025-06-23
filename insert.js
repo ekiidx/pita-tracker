92acbe553afaab8f3ccb8ea77c11d9c77beac16e
ekiidx <133534581+ekiidx@users.noreply.github.com>
2025-06-23 12:41:28 -0700

    added EJS template system

--- insert.js
+++ insert.js
@@ -1,23 +1,24 @@
 const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('./database.db', (err) => {
-  if (err) {
-    return console.error('Failed to connect to database:', err.message);
-  }
-  console.log('Connected to SQLite database.');
+    if (err) {
+        return console.error('Failed to connect to database:', err.message);
+    }
+    console.log('Connected to SQLite database.');
 });
 
 db.serialize(() => {
-  db.run(`DROP TABLE IF EXISTS blood`);
-  db.run(`DROP TABLE IF EXISTS insulin`);
-  db.run(`DROP TABLE IF EXISTS food`);
-  
-  db.run(`
+    db.run(`DROP TABLE IF EXISTS blood`);
+    db.run(`DROP TABLE IF EXISTS insulin`);
+    db.run(`DROP TABLE IF EXISTS food`);
+
+    db.run(`
     CREATE TABLE IF NOT EXISTS blood (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       date TEXT NOT NULL,
       time TEXT NOT NULL,
-      blood INTEGER NOT NULL
+      blood INTEGER NOT NULL,
+      temp INTEGER NOT NULL
     );
   `);
     db.run(`
@@ -37,38 +38,43 @@ db.serialize(() => {
     );
   `);
 
-    const stmt_blood = db.prepare(`INSERT INTO blood (date, time, blood) VALUES (?, ?, ?)`);
+    const stmt_blood = db.prepare(`INSERT INTO blood (date, time, blood, temp) VALUES (?, ?, ?, ?)`);
     const stmt_insulin = db.prepare(`INSERT INTO insulin (date, time, insulin) VALUES (?, ?, ?)`);
     const stmt_food = db.prepare(`INSERT INTO food (date, time, food) VALUES (?, ?, ?)`);
 
     const dummyBlood = [
-        ['2025-06-16', '01:00', '300'],
-        ['2025-06-16', '05:00', '200'],
+        ['2025-06-18', '09:00', '333', '74'],
+        ['2025-06-18', '12:00', '287', '75'],
+        ['2025-06-18', '16:00', '184', '77'],
+        ['2025-06-18', '19:00', '125', '76'],
     ];
     const dummyInsulin = [
-        ['2025-06-16', '01:00', '1'],
-        ['2025-06-16', '06:00', '1'],
+        ['2025-06-18', '10:00', '.5'],
+        ['2025-06-18', '12:45', '1'],
+        ['2025-06-18', '16:00', '.5'],
+        ['2025-06-18', '19:15', '1'],
     ];
     const dummyFood = [
-        ['2025-06-16', '01:00', '.5'],
-        ['2025-06-16', '09:00', '.5'],
+        ['2025-06-18', '09:30', '.25'],
+        ['2025-06-18', '12:30', '.5'],
+        ['2025-06-18', '17:00', '.25'],
     ];
 
-    dummyBlood.forEach(([date, time, blood]) => {
-    stmt_blood.run(date, time, blood);
-  });
+    dummyBlood.forEach(([date, time, blood, temp]) => {
+        stmt_blood.run(date, time, blood, temp);
+    });
     dummyInsulin.forEach(([date, time, insulin]) => {
-    stmt_insulin.run(date, time, insulin);
-  });
+        stmt_insulin.run(date, time, insulin);
+    });
     dummyFood.forEach(([date, time, food]) => {
-    stmt_food.run(date, time, food);
-  });
+        stmt_food.run(date, time, food);
+    });
 
     stmt_blood.finalize();
     stmt_insulin.finalize();
     stmt_food.finalize();
 
-  console.log('Database initialized with data.');
+    console.log('Database initialized with data.');
 });
 
 db.close();
